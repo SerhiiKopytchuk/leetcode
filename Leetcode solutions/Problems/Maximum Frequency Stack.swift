4318a539b521d7964f5bcdf4d23d6e2a023349db
Serhii <kopytchukserhii@gmail.com>
2023-04-06 11:37:43 +0300

    var -> let

--- Leetcode solutions/Problems/Maximum Frequency Stack.swift
+++ Leetcode solutions/Problems/Maximum Frequency Stack.swift
@@ -23,7 +23,7 @@ class FreqStack {
     /// space: O(1)
     func push(_ val: Int) {
         count[val, default: 0] += 1
-        var c = count[val, default: 0]
+        let c = count[val, default: 0]
         stacks[c, default:[]].append(val)
         maxCount = max(maxCount, c)
     }
@@ -31,7 +31,7 @@ class FreqStack {
     /// time: O(1)
     /// space: O(1)
     func pop() -> Int {
-        var res = stacks[maxCount, default: [0]].removeLast()
+        let res = stacks[maxCount, default: [0]].removeLast()
         count[res, default: 1] -= 1
         if stacks[maxCount, default:[]].isEmpty {
             maxCount -= 1
