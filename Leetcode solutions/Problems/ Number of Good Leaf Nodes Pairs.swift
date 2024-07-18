1f0e33b955206c2d5960d3b33aaf129cc10ab1a1
Serhii Kopytchuk <kopytchukserhii@gmail.com>
2024-07-18 12:55:48 +0300

    + 2

--- Leetcode solutions/Problems/ Number of Good Leaf Nodes Pairs.swift
+++ Leetcode solutions/Problems/ Number of Good Leaf Nodes Pairs.swift
@@ -6,6 +6,36 @@
 //
 
 import Foundation
+import Algorithms
+
+// time: O(n^2)
+// space: O(n)
+//func countPairs(_ root: TreeNode?, _ distance: Int) -> Int {
+//    var res = 0
+//
+//    func dfs(_ node: TreeNode?) -> [Int] {
+//        guard let node else { return [] }
+//
+//        if node.left == nil && node.right == nil {
+//            return [1]
+//        }
+//
+//        let leftArr = dfs(node.left)
+//        let rightArr = dfs(node.right)
+//
+//        for l in leftArr {
+//            for r in rightArr where l + r <= distance {
+//                res += 1
+//            }
+//        }
+//
+//        return leftArr.map { $0 + 1 } + rightArr.map { $0 + 1 }
+//    }
+//    _ = dfs(root)
+//
+//    return res
+//}
+
 
 // time: O(n^2)
 // space: O(n)
@@ -22,13 +52,9 @@ func countPairs(_ root: TreeNode?, _ distance: Int) -> Int {
         let leftArr = dfs(node.left)
         let rightArr = dfs(node.right)
 
-        for l in leftArr {
-            for r in rightArr where l + r <= distance {
-                res += 1
-            }
-        }
+        res += product(leftArr, rightArr).filter { $0 + $1 <= distance }.count
 
-        return leftArr.map { $0 + 1 } + rightArr.map { $0 + 1 }
+        return chain(leftArr, rightArr).map {$0 + 1}
     }
     _ = dfs(root)
 
---
+++ Leetcode solutions/Problems/Delete Nodes And Return Forest.swift
@@ -0,0 +1,33 @@
+//
+//  Delete Nodes And Return Forest.swift
+//  Problems
+//
+//  Created by Serhii Kopytchuk on 18.07.2024.
+//
+
+import Foundation
+
+// time: O(E+V)
+// space: O(n)
+func delNodes(_ root: TreeNode?, _ to_delete: [Int]) -> [TreeNode?] {
+    var delSet = Set(to_delete)
+    var res: [TreeNode?] = []
+
+    func dfs(_ node: TreeNode?) -> TreeNode? {
+        guard var node else { return nil }
+
+        node.left = dfs(node.left)
+        node.right = dfs(node.right)
+
+        if delSet.contains(node.val) {
+            res.append(node.left)
+            res.append(node.right)
+            return nil
+        }
+
+        return node
+    }
+    res.append(dfs(root))
+
+    return res.filter { $0 != nil }
+}
